d596aafe59779f6599deb78d568208fbabd559f7
ElifAltinbulak <76043798+ElifAltinbulak@users.noreply.github.com>
2021-03-10 21:40:38 +0300

    Update HW1.py

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,5 +1,22 @@
-#Explain your work
-
-#Question 1
-for x in range(a):
-	print(a)
+#HW1
+n = int(input("Please, Enter a limit number:"))
+number=list(range(n+1))
+even=[]
+odd=[]
+for i in number:
+    if i%2==0:
+        even.append(i)
+    else :
+        odd.append(i)
+new_list=even+odd
+even_end=[]
+odd_end=[]
+for x in new_list:
+    x*=2
+    if x%2==0:
+        even_end.append(x)
+    else:
+        odd_end.append(x)
+print(f"""Even Numbers: {even_end}
+Odd Numbers : {odd_end}""")
+        
